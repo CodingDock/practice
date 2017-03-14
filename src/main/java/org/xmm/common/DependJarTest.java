eb42a804e74f63031b768b6d9ffabf1a9ef61cc5
xiaoming <794084226@qq.com>
2017-03-14 16:52:18 +0800

    Initial commit

---
+++ src/main/java/org/xmm/common/DependJarTest.java
@@ -0,0 +1,26 @@
+package org.xmm.common;
+
+import com.xmm.App;
+/**
+ * Created by 肖明明 on 2017/3/14.
+ */
+
+/**
+ * 目录结构：
+ * runjar
+ * |--lib/
+ *    |-*.jar
+ * |--*.jar
+ * 运行命令： java -cp .;./lib/aaa.jar;practice.jar org.xmm.common.DependJarTest <br>
+ * 通配符版：java -cp .;./lib/*;* org.xmm.common.DependJarTest 
+ */
+
+public class DependJarTest {
+
+    public static void main(String[] args) {
+        
+        App app=new App();
+        app.main(null);
+        
+    }
+}
